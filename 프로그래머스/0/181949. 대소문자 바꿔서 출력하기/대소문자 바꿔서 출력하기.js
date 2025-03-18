de45387644c610210c8c375acdc57438d5a1e8ac
jiyongYoon <98104603+jiyongYoon@users.noreply.github.com>
2025-03-18 16:29:36 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 65.28 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -8,15 +8,17 @@ let input = [];
 
 rl.on('line', function (line) {
     input = [line];
-    let inputArr = input[0].split('');
-    console.log(inputArr.reduce((p, n) => {
-       let toLowerCase = n.toLowerCase();
-       if (n === toLowerCase) {
-           return p + n.toUpperCase();
-       } else {
-           return p + toLowerCase;
-       }
-    }, ''));
 }).on('close',function(){
     str = input[0];
+    let inputArr = str.split('');
+    let result = '';
+    for (const string of inputArr) {
+        let lowerCase = string.toLowerCase();
+        if (string === lowerCase) {
+            result += string.toUpperCase();
+        } else {
+            result += lowerCase;
+        }
+    }
+    console.log(result);
 });
